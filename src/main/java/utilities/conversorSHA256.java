3fe4e099a96385acca72ba4eae184e3de5701636
RafaLega <rafael.pereztorrecilla@gmail.com>
2022-03-05 18:18:54 +0100

    Creada utility para encriptar las contraseñas con SHA-256. closes #8

---
+++ src/main/java/utilities/conversorSHA256.java
@@ -0,0 +1,24 @@
+package utilities;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class conversorSHA256 {
+
+	public String convertirSHA256(String password) throws Exception{
+		MessageDigest md = null;
+		StringBuffer sb;
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+			byte [] hash = md.digest(password.getBytes());
+			sb = new StringBuffer();
+			for(byte b: hash) {
+				sb.append(String.format("%02x", b));
+			}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return sb.toString();
+	}
+}
