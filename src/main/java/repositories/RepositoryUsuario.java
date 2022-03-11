a6d725e9d2759dc7a094306ea0d4656574864a33
Beatriz M. Gordillo <beatriz.mgordillo@gmail.com>
2022-03-11 19:20:12 +0100

    Funcionalidad ADD del repositorio usuario creada. closes #12

--- src/main/java/repositories/RepositoryUsuario.java
+++ src/main/java/repositories/RepositoryUsuario.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.sinensia.contracts.UserDao;
 
@@ -52,8 +53,37 @@ public class RepositoryUsuario extends RepositoryBaseDatos implements UserDao<Us
 	}
 
 	@Override
-	public int add(Usuario modelo) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+	public Usuario add(Usuario usuario) throws SQLException {
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		int idUsuario = 0;
+		try {
+			connect = super.getconnection();
+			String sql = "INSERT * INTO usuario(nombre, correo, password)" + "VALUE (?, ?, ?)";
+			pst = connect.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			pst.setString(1, usuario.getNombre());
+			pst.setString(2, usuario.getCorreo());
+			pst.setString(3, usuario.getPassword());
+			pst.executeUpdate();
+			
+			rs = pst.getGeneratedKeys();
+			rs.next();
+			idUsuario = rs.getInt(1);
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (pst != null)
+				pst.close();
+			if (rs != null)
+				rs.close();
+			if (connect != null)
+				connect.close();
+		}
+		
+		usuario.setIdUsuario(idUsuario);
+		return usuario;
 	}
 }
