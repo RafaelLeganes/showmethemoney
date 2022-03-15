47b1f7e6aa63fe9d5d9ff77dbff3668186afa9ff
RafaLega <rafael.pereztorrecilla@gmail.com>
2022-03-15 22:57:24 +0100

    closes #74

---
+++ src/main/java/com/sinensia/models/Movimiento.java
@@ -0,0 +1,76 @@
+package com.sinensia.models;
+
+import java.time.LocalDate;
+
+public class Movimiento {
+	
+	private int idMovimiento;
+	private float importe;
+	private int idUsuario;
+	private int idCategoria;	
+	private String nombre;
+	private LocalDate fecha;
+	
+	public Movimiento() {
+		super();
+	}
+
+	public Movimiento(int idMovimiento, float importe, int idUsuario, int idCategoria, String nombre, LocalDate fecha) {
+		super();
+		this.idMovimiento = idMovimiento;
+		this.importe = importe;
+		this.idUsuario = idUsuario;
+		this.idCategoria = idCategoria;
+		this.nombre = nombre;
+		this.fecha = fecha;
+	}
+
+	public int getIdMovimiento() {
+		return idMovimiento;
+	}
+
+	public void setIdMovimiento(int idMovimiento) {
+		this.idMovimiento = idMovimiento;
+	}
+
+	public float getImporte() {
+		return importe;
+	}
+
+	public void setImporte(float importe) {
+		this.importe = importe;
+	}
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+	
+}
