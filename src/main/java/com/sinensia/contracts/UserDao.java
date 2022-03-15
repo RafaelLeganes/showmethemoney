4eb3b0853d223f646cd8cc948d855e76ea9fbd30
RafaLega <rafael.pereztorrecilla@gmail.com>
2022-03-15 22:41:30 +0100

    closes #56

--- src/main/java/com/sinensia/contracts/UserDao.java
+++ src/main/java/com/sinensia/contracts/UserDao.java
@@ -1,12 +1,15 @@
 package com.sinensia.contracts;
 
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Properties;
+
 
 
 public interface UserDao<T> {
 
 	public T get(String nombre, String pass) throws SQLException;
-	
-	public T add(T modelo) throws SQLException;
+
+	public T add(T modelo, Properties appProps) throws SQLException, IOException;
 
 }
